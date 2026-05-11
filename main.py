5bfd29a8ad08d64e62621448d693310ca3ea5ec3
cokear <138343973+cokear@users.noreply.github.com>
2026-05-11 15:17:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=87a60ff5-ed94-460b-a3c3-3317550528b6",
     # 添加更多链接
 ]
 
